42278b12320893a6ffdc80f9d572002ea89375d7
Fernando Cárdenas <ferca@Fernandos-MacBook-Air.local>
2019-08-22 02:31:30 -0500

    news and survey pages

--- src/pages/news.vue
+++ src/pages/news.vue
@@ -5,16 +5,50 @@
             Noticias
         </f7-block-title>
 
-        <f7-block>
-
-        </f7-block>
+        <f7-card v-for="(item, index) in news" :key="index">
+            <f7-card-content>
+                {{item.text}}
+            </f7-card-content>
+        </f7-card>
 
     </f7-page>
 </template>
 
 <script>
     export default {
-        name: "news"
+        name: "news",
+        data() {
+            return {
+                news: []
+            }
+        },
+        created: function () {
+        },
+        mounted: function () {
+            this.getNews()
+        },
+        methods: {
+            getNews() {
+                this.news = []
+
+                this.$f7.dialog.preloader('Cargando Datos');
+                this.$http.post(this.$store.state.application.config.api + 'news').then(response => {
+                    this.news = response.body;
+
+                    if (this.news.length === 0) {
+                        this.news = [{text:'Sin noticias por el momento'}]
+                        this.$f7.dialog.alert(' ', 'Sin datos disponibles');
+                    }
+                    this.$f7.dialog.close();
+                }, response => {
+                    this.news = [{text:'Sin noticias por el momento'}]
+                    // error callback
+                    console.log(response, 'error on getNews');
+                    this.$f7.dialog.alert(' ', 'Servidor no disponible');
+                    this.$f7.dialog.close();
+                });
+            },
+        }
     }
 </script>
 
--- src/pages/survey.vue
+++ src/pages/survey.vue
@@ -12,6 +12,52 @@
                 {{survey.title}}
             </f7-button>
         </f7-block>
+        <f7-popup class="answers-popup-swipe" swipe-to-close>
+            <f7-page>
+                <f7-navbar :title="tempAnswers.title">
+                    <f7-nav-right>
+                        <f7-link popup-close>Cerrar</f7-link>
+                    </f7-nav-right>
+                </f7-navbar>
+
+                <f7-block inset style="background-color: rgba(255,255,255,.84); border-radius: 8px; margin-top: 10px">
+                    <p><strong>Detalles:</strong> {{tempAnswers.details}}</p>
+                </f7-block>
+
+                <f7-card v-for="(question, index) in tempAnswers.questions" :key="index">
+                    <f7-block style="background-color: #368da8; border-radius: 4px">
+                        <f7-row>
+                            <p style="text-align: left">
+                                <span class="s-subtitle" style="color:white !important">
+                                    {{ question.title }}
+                                </span>
+                            </p>
+                        </f7-row>
+                    </f7-block>
+                    <f7-list inline-labels>
+                        <f7-list-input
+                                type="select"
+                                :value="question.answer"
+                                @input="question.answer = $event.target.value"
+                        >
+                            <option v-for="answer in tempAnswers.answers[index]" :value="answer">
+                                {{ answer }}
+                            </option>
+                        </f7-list-input>
+
+                    </f7-list>
+                </f7-card>
+
+                <f7-block>
+                    <f7-button style="background-color: #f49931; height: 60px; font-size: 1.2em;"  @click="sendForm">
+                        <p style="font-weight: bold;color:white;">
+                            Enviar
+                        </p>
+                    </f7-button>
+                </f7-block>
+
+            </f7-page>
+        </f7-popup>
 
     </f7-page>
 </template>
@@ -21,50 +67,84 @@
         name: "survey",
         data() {
             return {
-                tempAnswers: [],
-                surveys: [
-                    {
-                        id: 0,
-                        title: '¿Está de acuerdo con blahblah?',
-                        questions: [
-                            {
-                                id: 0,
-                                title: 'lalelilolu?',
-                                answers: ['lol', 'lal', 'lel',]
-                            },
-                            {
-                                id: 1,
-                                title: 'lalelilolu?',
-                                answers: ['lol', 'lal', 'lel',]
-                            }
-                        ],
-                    },{
-                        id: 1,
-                        title: '¿Está de acuerdo con blahblah?',
-                        questions: [
-                            {
-                                id: 0,
-                                title: 'lalelilolu?',
-                                answers: ['lol', 'lal', 'lel',]
-                            },
-                            {
-                                id: 1,
-                                title: 'lalelilolu?',
-                                answers: ['lol', 'lal', 'lel',]
-                            }
-                        ],
-                    }
-                ],
-
+                tempAnswers: {
+                    title: '',
+                    details: '',
+                    answers: [],
+                    questions: []
+                },
+                surveys: [],
             }
         },
         created: function () {
         },
         mounted: function () {
+            this.getSurveys()
         },
         methods: {
+            sendForm() {
+                const self = this;
+                const app = self.$f7;
+                if (self.tempAnswers.questions.find(self.checkBeforeSendForm)) {
+                    app.dialog.alert(
+                        "Debes de completar todos los datos",
+                        'Verifica tus respuestas'
+                    )
+                } else {
+                    app.dialog.preloader('Enviando Encuesta');
+                    this.$http.post(this.$store.state.application.config.api + 'addAnswers', {
+                        //user_id: this.$store.state.application.user.id,
+                        questions: this.tempAnswers.questions
+                    }).then(response => {
+                        app.dialog.close();
+                        // get body data
+                        let data = response.body
+                        if(data.error == 0){
+                            app.dialog.alert(
+                                "¡Muchas gracias!",
+                                'Encuesta enviada'
+                            )
+                        } else if(data.error == 1){
+                            app.dialog.alert(
+                                "Encuesta enviada anteriormente",
+                                '¡Muchas gracias!'
+                            )
+                        }
+                    }, response => {
+                        app.dialog.close();
+                        app.dialog.alert(
+                            "Intenta más tarde",
+                            'Servidor no disponible'
+                        )
+                        console.log(response, 'error on sendForm')
+                    });
+                }
+            },
+            checkBeforeSendForm(arg) {
+                if (arg.answer === this.tempAnswers.answers[0][0]) {
+                    return true
+                } else {
+                    return false
+                }
+            },
             getSurveyItemById(id) {
-                console.log(id, 'id')
+                this.$f7.dialog.preloader('Cargando Datos');
+                this.$http.post(this.$store.state.application.config.api + 'answers',
+                    { id: id }
+                ).then(response => {
+                    this.tempAnswers = response.body;
+                    this.$f7.dialog.close();
+                    if (this.tempAnswers.length === 0) {
+                        this.$f7.dialog.alert(' ', 'Sin datos disponibles');
+                    } else {
+                        this.$f7.popup.open('.answers-popup-swipe', true)
+                    }
+                }, response => {
+                    // error callback
+                    this.$f7.dialog.alert(' ', 'Servidor no disponible');
+                    console.log(response, 'error on getSurveys');
+                    this.$f7.dialog.close();
+                });
             },
             getSurveys() {
                 this.$f7.dialog.preloader('Cargando Datos');
@@ -73,12 +153,14 @@
                     this.surveys = response.body;
 
                     if (this.surveys.length === 0) {
+                        this.surveys = [{text: 'Sin encuestas por el momento'}]
                         this.$f7.dialog.alert(' ', 'Sin datos disponibles');
                     }
                     this.$f7.dialog.close();
                 }, response => {
-                    this.$f7.dialog.alert(' ', 'Servidor no disponible');
                     // error callback
+                    this.surveys = [{text: 'Sin encuestas por el momento'}]
+                    this.$f7.dialog.alert(' ', 'Servidor no disponible');
                     console.log(response, 'error on getSurveys');
                     this.$f7.dialog.close();
                 });
